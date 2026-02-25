0bb0503e0a39f4aa412d3cf245239ec81db7fb87
ardhikaxx <ardhikayanuar58@gmail.com>
2026-02-25 15:46:34 +0700

    feat(config): add routes and rupiah blade directive

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,58 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\SalaryStructureController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\LeaveTypeController;
+use App\Http\Controllers\PayrollController;
+use App\Http\Controllers\ChartOfAccountController;
+use App\Http\Controllers\JournalEntryController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\BillController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\BudgetController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('roles', RoleController::class);
+    Route::resource('permissions', PermissionController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('departments', DepartmentController::class);
+
+    Route::resource('employees', EmployeeController::class);
+    Route::resource('salary-structures', SalaryStructureController::class);
+    Route::resource('attendances', AttendanceController::class);
+    Route::resource('leaves', LeaveController::class);
+    Route::resource('leave-types', LeaveTypeController::class);
+    Route::resource('payrolls', PayrollController::class);
+    Route::post('/leaves/{leaveApplication}/approve', [LeaveController::class, 'approve'])->name('leaves.approve');
+    Route::post('/leaves/{leaveApplication}/reject', [LeaveController::class, 'reject'])->name('leaves.reject');
+
+    Route::resource('accounts', ChartOfAccountController::class);
+    Route::resource('journal-entries', JournalEntryController::class);
+    Route::resource('invoices', InvoiceController::class);
+    Route::post('/invoices/{invoice}/mark-paid', [InvoiceController::class, 'markAsPaid'])->name('invoices.markPaid');
+    Route::resource('bills', BillController::class);
+    Route::post('/bills/{bill}/mark-paid', [BillController::class, 'markAsPaid'])->name('bills.markPaid');
+    Route::resource('customers', CustomerController::class);
+    Route::resource('vendors', VendorController::class);
+    Route::resource('budgets', BudgetController::class);
 });
